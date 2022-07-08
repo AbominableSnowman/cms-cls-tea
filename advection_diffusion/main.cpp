1ec78c4e1c66419c40632c42044b0ee6d73049be
Jarryd <calhounjarryd.gmail.com>
2022-07-08 11:43:46 +0200

    Reorganized code for greater efficiency & added source/sink expansion

--- advection_diffusion/main.cpp
+++ advection_diffusion/main.cpp
@@ -21,9 +21,9 @@ int main(int argc, char* argv[])
 	// Define output locations & experiment values
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	bool diffusion_on = true;
-	bool growth_on = false;
+	bool growth_on = true;
 	bool advection_on = false;
-	int source_sink_cond = 4; // 1-4 different testing scenarios for source & sink locations
+	int source_sink_cond = 1; // 1-4 different testing scenarios for source & sink locations
 	/*	1: Source on bottom, sink on top
 		2: Sources and sinks covering the entire disk
 		3: Source in center, sink on boundary
@@ -65,13 +65,10 @@ int main(int argc, char* argv[])
 	const double D = 0.1; // diffusion constant
 	double k_source = 1;
 	double k_sink   = 1;
-    double emb_boundary = 0; // embryo boundary; used to define source & sink locations
 	double mu [dims]    = {box_upper/2.0, box_upper/2.0}; // For initial gaussian conc field
 	double sigma [dims] = {box_upper/10.0, box_upper/10.0}; // For initial gaussian conc field
-		
 	
-	// Advection values
-	//const double v = 0.1; // velocity
+	// Growth values
 	const double v[dims] = {0.1, 0.1};
 	
 	// Grid property indices		
@@ -87,7 +84,11 @@ int main(int argc, char* argv[])
 		V_SIGN                 = 8,  // sign of velocity, needed for the upwinding
 		PHI_GRAD               = 9,  // gradient of phi (vector field)
 		PHI_GRAD_MAGNITUDE     = 10; // Eucledian norm of gradient (scalar field)
-			
+	
+	// Signed distance function values
+	double emb_boundary = 0; // embryo boundary: where SDF ~= 0
+	typedef double phi_type;
+	auto phi_epsilon = std::numeric_limits<phi_type>::epsilon();
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// OpenFPM Grid Initialization
@@ -153,14 +154,13 @@ int main(int argc, char* argv[])
     std::cout << "phi is: " << phi_max << std::endl;
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Set source & sink locations
+	// Set source & sink locations	
 	auto dom_src_snk = g_dist.getDomainIterator();
-	typedef double phi_type;
 	while(dom_src_snk.isNext()) {
 		auto key = dom_src_snk.get(); // index of current grid node
 		auto phi_here = g_dist.template getProp<PHI_N>(key);
 		
-		if (phi_here >= emb_boundary - std::numeric_limits<phi_type>::epsilon()) {
+		if (phi_here >= emb_boundary - phi_epsilon) {
             // Scenario 1
 			if (source_sink_cond == 1){
 				// get coordinates of grid point
@@ -247,7 +247,7 @@ int main(int argc, char* argv[])
 	// Final dt is min of the two:
 	const double dt = std::min(dt_adv, dt_dif);
 	// Save initial state
-	g_dist.write(path_output + "grid_initial", FORMAT_BINARY);
+	if (save_vtk){g_dist.write(path_output + "grid_initial", FORMAT_BINARY);}
 	
 	
 	
@@ -262,32 +262,67 @@ int main(int argc, char* argv[])
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	while(iter < max_iter) // looping over time
 	{
+		// Update field calculations for whole grid
+		if (growth_on){
+			get_upwind_gradient<PHI_N, V_SIGN, PHI_GRAD>(g_dist, 1, true);
+			get_vector_magnitude<PHI_GRAD, PHI_GRAD_MAGNITUDE, double>(g_dist);
+			int phi_grad_tol_break = 0; // counts the # of grid points outside of tolerance range
+		}
+		if (diffusion_on){
+			get_laplacian_grid<CONC_N, CONC_LAP>(g_dist);
+			get_upwind_gradient<CONC_N, V_SIGN, CONC_N_GRAD>(g_dist, 1, true);
+		}
+		
+		// Grid maintenance: no-flux boundary, source/sink expansion, SDF distortion
+		auto dom4 = g_dist.getDomainIterator();
+		int phi_grad_tol_break = 0;
+		while(dom4.isNext()) {
+			auto key = dom4.get();
+			auto phi_here = g_dist.template getProp<PHI_N>(key);
+			
+			if(phi_here >= emb_boundary - phi_epsilon) {
+				for(int d = 0; d < dims; ++d) {
+					if(g_dist.template get<PHI_N>(key.move(d, 1)) < emb_boundary + phi_epsilon) {
+						// Impose no-flux boundary
+						g_dist.template get<CONC_N>(key.move(d, 1)) = g_dist.template get<CONC_N>(key);
+						
+						// Expand sources / sinks
+						if (g_dist.template get<K_SOURCE>(key.move(d, 1)) == k_source){
+							g_dist.template get<K_SOURCE>(key) = k_source;
+						}
+						if (g_dist.template get<K_SINK>(key.move(d, 1)) == k_sink){
+							g_dist.template get<K_SINK>(key) = k_sink;
+						}
+					}
+					if(g_dist.template get<PHI_N>(key.move(d, -1)) < emb_boundary + phi_epsilon) {
+						// Impose no-flux boundary
+						g_dist.template get<CONC_N>(key.move(d, -1)) = g_dist.template get<CONC_N>(key);
+						
+						// Expand sources / sinks
+						if (g_dist.template get<K_SOURCE>(key.move(d, -1)) == k_source){
+							g_dist.template get<K_SOURCE>(key) = k_source;
+						}
+						if (g_dist.template get<K_SINK>(key.move(d, -1)) == k_sink){
+							g_dist.template get<K_SINK>(key) = k_sink;
+						}	
+					}
+				}
+				////////////////////////////////////////////////////////////////////////////////////////////////////////
+				// Determine if redistancing needs to be run based on # of grid points 
+				if (growth_on){
+					auto phi_grad_mag = g_dist.template get<PHI_GRAD_MAGNITUDE>(key); //get the magnitude of the phi gradient
+					if (phi_grad_mag > 1.2 || phi_grad_mag < 0.8){
+						phi_grad_tol_break += 1;
+					}
+				}
+			}
+			++dom4;
+		}
+		
+		
 		
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		if (diffusion_on){
-			// Impose no-flux BCs at the disk interface
-			auto dom4 = g_dist.getDomainIterator();
-			while(dom4.isNext()) {
-				auto key = dom4.get();
-				if(g_dist.template getProp<PHI_N>(key) >= emb_boundary - std::numeric_limits<phi_type>::epsilon()) {
-					for(int d = 0; d < dims; ++d) {
-						if(g_dist.template get<PHI_N>(key.move(d, 1)) < emb_boundary + std::numeric_limits<double>::epsilon()) {
-							g_dist.template get<CONC_N>(key.move(d, 1)) = g_dist.template get<CONC_N>(key);
-						}
-						
-						if(g_dist.template get<PHI_N>(key.move(d, -1)) < emb_boundary + std::numeric_limits<double>::epsilon()) {
-							g_dist.template get<CONC_N>(key.move(d, -1)) = g_dist.template get<CONC_N>(key);
-						}
-					}
-				}
-				++dom4;
-			}
-			
-			// Compute laplacian of concentration for whole grid
-			get_laplacian_grid<CONC_N, CONC_LAP>(g_dist);
-			// Compute gradient of concentration for whole grid
-			get_upwind_gradient<CONC_N, V_SIGN, CONC_N_GRAD>(g_dist, 1, true);
-
 			// Update grid points by solving ODE 
 			auto dom3 = g_dist.getDomainIterator();
 			while (dom3.isNext()) {
@@ -299,63 +334,26 @@ int main(int argc, char* argv[])
 				auto ksource_here = g_dist.template get<K_SOURCE>(key);
 				auto ksink_here = g_dist.template get<K_SINK>(key);
 				
-				
 				if (phi_here >= emb_boundary - epsilon_phi){
 					
 					if (diffusion_on && !advection_on){
-						g_dist.template get<CONC_NPLUS1>(key) = u
-																+ D*dt*laplacian_u
-																+ dt*ksource_here
-																- dt*ksink_here*u;            
+						g_dist.template get<CONC_NPLUS1>(key) = u + D*dt*laplacian_u 
+																+ dt*ksource_here - dt*ksink_here*u;            
 					}
 					else if (diffusion_on && advection_on){
 						auto del_u = g_dist.template get<CONC_N_GRAD>(key);
-						double v_advec = 0.;
+						double v_advec = 0;
 						for(size_t d = 0; d < dims; d++){v_advec += g_dist.template get<CONC_N_GRAD>(key)[d] * v[d];}
-						g_dist.template get<CONC_NPLUS1>(key) = u
-																+ D*dt*laplacian_u
-																+ dt*ksource_here
-																- dt*ksink_here*u
-																+ dt*v_advec;
+						g_dist.template get<CONC_NPLUS1>(key) = u+ D*dt*laplacian_u + dt*ksource_here 
+																- dt*ksink_here*u + dt*v_advec;
 					}
 				}
 				++dom3;
 			}
-			// Update CONC_N
-			copy_gridTogrid<CONC_NPLUS1, CONC_N>(g_dist, g_dist);
+			copy_gridTogrid<CONC_NPLUS1, CONC_N>(g_dist, g_dist); // Update CONC_N
 		} // End diffusion
-		
-		
-		
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		if (growth_on){
-			/* Advection: check condition of phi gradient. Count the number of points where PHI_GRAD > upper-bound 
-			tolerance OR PHI_GRAD < lower-bound tolerance and if # of points out of range > tolerance value, then run 
-			sussman redistancing to reinitialize phi.*/
-			
-			// Compute upwind gradient of phi for whole grid
-			get_upwind_gradient<PHI_N, V_SIGN, PHI_GRAD>(g_dist, 1, true); // the upwind gradient is automatically
-			// one-sided at the boundary
-			get_vector_magnitude<PHI_GRAD, PHI_GRAD_MAGNITUDE, double>(g_dist);
-			// Loop over grid and simulate growth using the surface normals (= magnitude gradient of phi) computed above
-			// This just runs over the whole box so far
-			
-			// Determine if redistancing needs to be run based on # of grid points 
-			int phi_grad_tol_break = 0; // counts the # of grid points outside of tolerance range
-			auto phi_grad_dom = g_dist.getDomainIterator();
-			/*while (phi_grad_dom.isNext()){
-				auto key = phi_grad_dom.get();
-				auto phi_here = g_dist.template getProp<PHI_N>(key);
-				
-				if (phi_here < emb_boundary - 0.2) {
-					auto phi_grad_mag = g_dist.template get<PHI_GRAD_MAGNITUDE>(key); //get the magnitude of the phi gradient
-					if (phi_grad_mag > 1.2 || phi_grad_mag < 0.8){
-						phi_grad_tol_break += 1;
-					}
-				}
-				++phi_grad_dom;
-			}*/
-			
 			if (phi_grad_tol_break > 10) {	
 				Redist_options<phi_type> redist_options;
 				redist_options.min_iter                             = 1e3;
@@ -372,12 +370,13 @@ int main(int argc, char* argv[])
 				redist_options.print_steadyState_iter               = true;
 				redist_options.save_temp_grid                       = true;
 				
-				RedistancingSussman<grid_type, phi_type> redist_obj(g_dist, redist_options); // Instantiation of Sussman-redistancing class
-				redist_obj.run_redistancing<PHI_N, PHI_N>(); //update phi_n using sussman redistancing
-				get_upwind_gradient<PHI_N, V_SIGN, PHI_GRAD>(g_dist, 1, true); //calculate new gradient and magnitude using new phi_n
+				// Instantiation of Sussman-redistancing class
+				RedistancingSussman<grid_type, phi_type> redist_obj(g_dist, redist_options); 
+				//Update SDF using Sussman Redistancing 
+				redist_obj.run_redistancing<PHI_N, PHI_N>(); 
+				// Calculate new gradient and magnitude using new phi_n
+				get_upwind_gradient<PHI_N, V_SIGN, PHI_GRAD>(g_dist, 1, true); 
 				get_vector_magnitude<PHI_GRAD, PHI_GRAD_MAGNITUDE, double>(g_dist);
-				//auto phi_gra_mag_2 = g_dist.template get<PHI_GRAD_MAGNITUDE>(dom.get()); //get the magnitude of the phi gradient 
-				//std::cout << "phi_gra_mag_2:" << phi_gra_mag_2 << std::endl;
 			} 
 			
 			// Set PHI_NPLUS1 to PHI_N and update PHI_N
@@ -390,7 +389,6 @@ int main(int argc, char* argv[])
 			}
 			// Update PHI_N
 			copy_gridTogrid<PHI_NPLUS1, PHI_N>(g_dist, g_dist);
-			
 		} // End growth
 		
 		
