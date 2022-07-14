3e4192ab6d568f00566f92491d78c162abd804bc
Jarryd <calhounjarryd.gmail.com>
2022-07-14 11:58:27 +0200

    Fixed advection (the wrong way); still need to fix per today's discussion.

--- advection_diffusion/main.cpp
+++ advection_diffusion/main.cpp
@@ -23,8 +23,10 @@ int main(int argc, char* argv[])
 	bool diffusion_on = true;
 	bool growth_on = true;
 	bool advection_on = true;
+	bool gaussian_ic = true;
 	int source_sink_cond = 1; // 1-4 different testing scenarios for source & sink locations
-	/*	1: Source on bottom, sink on top
+	/*	0: No sources or sinks
+		1: Source on bottom, sink on top
 		2: Sources and sinks covering the entire disk
 		3: Source in center, sink on boundary
 		4: Sources and sinks covering either side of embryo (separated across x-axis)*/
@@ -33,6 +35,7 @@ int main(int argc, char* argv[])
 	bool save_vtk = true;
 	bool save_hdf5 = false;
 	bool save_mass = false;
+	bool save_csv = true;
 	std::string cwd = get_cwd();
 	const std::string path_output = cwd + "/output_advection_diffusion/";
 	create_directory_if_not_exist(path_output);
@@ -46,10 +49,8 @@ int main(int argc, char* argv[])
 	// Simulation & grid values
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Temporal values
-	double t = 0;
-	int iter = 0; // initial iteraton
 	int max_iter = 1e3; // max iteration --> be careful, that the box is large enough to contain the growing disk!
-	int interval_write = (int)(max_iter / 200); // set how many frames should be saved as vtk
+	int interval_write = (int)(max_iter / 100); // set how many frames should be saved as vtk
 	
 	// Spatial values
 	const size_t dims = 2; // Grid dimensions
@@ -65,11 +66,11 @@ int main(int argc, char* argv[])
 	const double D = 0.1; // diffusion constant
 	double k_source = 1;
 	double k_sink   = 1;
-	double mu [dims]    = {box_upper/2.0, box_upper/2.0}; // For initial gaussian conc field
-	double sigma [dims] = {box_upper/10.0, box_upper/10.0}; // For initial gaussian conc field
+	double mu[dims]    = {box_upper/2.0, box_upper/2.0}; // For initial gaussian conc field
+	double sigma[dims] = {box_upper/10.0, box_upper/10.0}; // For initial gaussian conc field
 	
 	// Growth values
-	const double v[dims] = {0.4, 0.4};
+	const double v[dims] = {0.2, 0.2};
 	
 	// Grid property indices		
 	constexpr size_t
@@ -126,7 +127,7 @@ int main(int argc, char* argv[])
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Initialize source, sink, & concentration field
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// find the biggest phi value and its location + max x/y coordinates in embryo
+	// Find the biggest phi value and its location + max x/y coordinates in embryo
 	double phi_max = 0;
 	double x_max = 0;
     double y_max = 0;
@@ -154,17 +155,22 @@ int main(int argc, char* argv[])
     std::cout << "phi is: " << phi_max << std::endl;
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Set source & sink locations	
-	auto dom_src_snk = g_dist.getDomainIterator();
-	while(dom_src_snk.isNext()) {
-		auto key = dom_src_snk.get(); // index of current grid node
+	// Set source & sink locations & initialize concentration field
+	auto init_dom_iter = g_dist.getDomainIterator();
+	while(init_dom_iter.isNext()) {
+		auto key = init_dom_iter.get(); // index of current grid node
 		auto phi_here = g_dist.template getProp<PHI_N>(key);
+		Point<grid_type::dims, typename grid_type::stype> coords = g_dist.getPos(key);
 		
 		if (phi_here >= emb_boundary - phi_epsilon) {
-            // Scenario 1
+			////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// Concentration initial conditions
+			if (gaussian_ic){
+				g_dist.template get<CONC_N>(key)= gaussian(coords, mu, sigma);
+			}
+			////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// Sources and sinks
 			if (source_sink_cond == 1){
-				// get coordinates of grid point
-				Point<grid_type::dims, typename grid_type::stype> coords = g_dist.getPos(key); 
 				auto x_coord = coords.get(0);
 				auto y_coord = coords.get(1);
 				
@@ -181,12 +187,10 @@ int main(int argc, char* argv[])
 					g_dist.template get<K_SINK>(key) = 0;
 				}
 			}
-			// Scenario 2
 			else if (source_sink_cond == 2){
 				g_dist.template get<K_SOURCE>(key) = k_source;
 				g_dist.template get<K_SINK>(key) = k_sink;
 			}
-			// Scenario 3
 			else if (source_sink_cond == 3){
 				if(phi_here < 0.1*phi_max){
 					g_dist.template get<K_SOURCE>(key) = 0;
@@ -203,8 +207,6 @@ int main(int argc, char* argv[])
 			}
 			// Scenario 4
 			else if (source_sink_cond == 4){
-				// get coordinates of grid point
-				Point<grid_type::dims, typename grid_type::stype> coords = g_dist.getPos(key); 
 				auto x_coord = coords.get(0);
 				auto y_coord = coords.get(1);
 				
@@ -221,8 +223,9 @@ int main(int argc, char* argv[])
 					g_dist.template get<K_SINK>(key) = 0;
 				}
 			}
+			////////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
-		++dom_src_snk;
+		++init_dom_iter;
 	}
 	
 	// Check and print initial mass after initializing concentrations
@@ -237,20 +240,41 @@ int main(int argc, char* argv[])
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Stability Conditions
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Advection stability condition
-	auto max_v = *std::max_element(std::begin(v), std::end(v));
-	const double dt_adv = get_advection_time_step_cfl(g_dist, max_v, 0.1);
-	std::cout << "dt (advection) = " << dt_adv << std::endl;
 	// Diffusion stability condition
 	const double dt_dif = get_diffusion_time_step(g_dist, D);
-	std::cout << "dt (diffusion) = " << dt_dif << std::endl;
-	// Final dt is min of the two:
-	const double dt = std::min(dt_adv, dt_dif);
+	const double dt = dt_dif;
+	if (growth_on || advection_on){
+		//auto max_v = *std::max_element(std::begin(v), std::end(v));
+		//const double dt_adv = get_advection_time_step_cfl(g_dist, max_v, 0.1);
+		double sum = 0;
+		for (size_t d = 0; d < dims; d++)
+		{
+			sum += v[d] / g_dist.spacing(d);
+		}
+		const double dt_adv = 0.1 / abs(sum);
+		
+		std::cout << "dt (advection) = " << dt_adv << std::endl;
+		std::cout << "dt (diffusion) = " << dt_dif << std::endl;
+		// Final dt is min of the two
+		const double dt = std::min(dt_adv, dt_dif);
+	}
+	std::cout << "dt = " << dt << std::endl;
+	
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Saving
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Save initial state
 	if (save_vtk){g_dist.write(path_output + "grid_initial", FORMAT_BINARY);}
-	
-	
-	
+	// Initialize csv writer
+	std::ofstream file_out;
+	std::string csv_row = "";
+	if (save_csv){
+		// but don't create file unless save_csv is on
+		std::string csv_path_output = save_path + ".csv";
+		create_file_if_not_exist(csv_path_output);
+		file_out.open(csv_path_output, std::ios_base::app); // append instead of overwrite
+	}
 	
 	
 	
@@ -260,6 +284,8 @@ int main(int argc, char* argv[])
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Simulation
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	double t = 0;
+	int iter = 0;
 	while(iter < max_iter) // looping over time
 	{
 		// Update field calculations for whole grid
@@ -271,14 +297,17 @@ int main(int argc, char* argv[])
 			get_laplacian_grid<CONC_N, CONC_LAP>(g_dist);
 			get_upwind_gradient<CONC_N, V_SIGN, CONC_N_GRAD>(g_dist, 1, true);
 		}
-		
+		if (save_csv && iter % interval_write == 0){csv_row = to_string_with_precision(t, 6);} // start of new row in csv file
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// Grid maintenance: no-flux boundary, source/sink expansion, SDF distortion
-		auto dom4 = g_dist.getDomainIterator();
+		auto maintenance_dom_iter = g_dist.getDomainIterator();
 		int phi_grad_tol_break = 0; // counts the # of grid points outside of tolerance range
-		while(dom4.isNext()) {
-			auto key = dom4.get();
+		while(maintenance_dom_iter.isNext()) {
+			auto key = maintenance_dom_iter.get();
 			auto phi_here = g_dist.template getProp<PHI_N>(key);
-			
+			if (save_csv && iter % interval_write == 0){
+				csv_row += "," + to_string_with_precision(g_dist.template get<CONC_N>(key));
+				}
 			if(phi_here >= emb_boundary - phi_epsilon) {
 				for(int d = 0; d < dims; ++d) {
 					if(g_dist.template get<PHI_N>(key.move(d, 1)) < emb_boundary + phi_epsilon) {
@@ -319,7 +348,7 @@ int main(int argc, char* argv[])
 					}
 				}
 			}
-			++dom4;
+			++maintenance_dom_iter;
 		}
 		
 		
@@ -327,9 +356,9 @@ int main(int argc, char* argv[])
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		if (diffusion_on){
 			// Update grid points by solving ODE 
-			auto dom3 = g_dist.getDomainIterator();
-			while (dom3.isNext()) {
-				auto key = dom3.get();
+			auto diff_dom_iter = g_dist.getDomainIterator();
+			while (diff_dom_iter.isNext()) {
+				auto key = diff_dom_iter.get();
 				auto phi_here = g_dist.template getProp<PHI_N>(key);
 				auto u = g_dist.template get<CONC_N>(key);
 				auto laplacian_u = g_dist.template get<CONC_LAP>(key);
@@ -344,14 +373,14 @@ int main(int argc, char* argv[])
 																+ dt*ksource_here - dt*ksink_here*u;            
 					}
 					else if (diffusion_on && advection_on){
-						auto del_u = g_dist.template get<CONC_N_GRAD>(key);
+						auto grad_u = g_dist.template get<CONC_N_GRAD>(key);
 						double v_advec = 0;
-						for(size_t d = 0; d < dims; d++){v_advec += g_dist.template get<CONC_N_GRAD>(key)[d] * v[d];}
+						for(size_t d = 0; d < dims; d++){v_advec += abs(grad_u[d]) * v[d];}
 						g_dist.template get<CONC_NPLUS1>(key) = u+ D*dt*laplacian_u + dt*ksource_here 
 																- dt*ksink_here*u + dt*v_advec;
 					}
 				}
-				++dom3;
+				++diff_dom_iter;
 			}
 			copy_gridTogrid<CONC_NPLUS1, CONC_N>(g_dist, g_dist); // Update CONC_N
 		} // End diffusion
@@ -383,12 +412,12 @@ int main(int argc, char* argv[])
 			}
 			
 			// Set PHI_NPLUS1 to PHI_N and update PHI_N
-			auto phi_n_dom = g_dist.getDomainIterator();
-			while(phi_n_dom.isNext()) // looping over the grid points
+			auto phi_dom_iter = g_dist.getDomainIterator();
+			while(phi_dom_iter.isNext()) // looping over the grid points
 			{
-				auto key = phi_n_dom.get();
+				auto key = phi_dom_iter.get();
 				g_dist.template get<PHI_NPLUS1>(key) = g_dist.template get<PHI_N>(key) + dt * v[0] * g_dist.template get<PHI_GRAD_MAGNITUDE>(key);
-				++phi_n_dom;
+				++phi_dom_iter;
 			}
 			// Update PHI_N
 			copy_gridTogrid<PHI_NPLUS1, PHI_N>(g_dist, g_dist);
@@ -404,11 +433,13 @@ int main(int argc, char* argv[])
 			if (save_vtk){g_dist.write_frame(save_path, iter, FORMAT_BINARY);}
 			if (save_hdf5){g_dist.save(save_path + std::to_string(iter) + ".hdf5");}
 			//if (save_mass){monitor_absolute_mass_over_region<CONC_N, PHI_N>(g_dist, emb_boundary, m_initial, p_volume, t, iter, path_output, save_path + "mass.csv");}
+			if (save_csv){file_out << csv_row << std::endl;}
 		}
 		std::cout << "Time :" << t << ", iteration: " << std::to_string(iter) << std::endl;
 		iter += 1;
 		t += dt;
 	}
+	file_out.close();
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// End simulation
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
