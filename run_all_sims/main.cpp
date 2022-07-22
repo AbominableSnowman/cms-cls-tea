8682f9ab9a54d9cabfb7dc90a2d279895d007649
Jarryd <calhounjarryd.gmail.com>
2022-07-22 10:40:59 +0200

    Updated with code from advection_diffusion where bulging problem was solved

--- run_all_sims/main.cpp
+++ run_all_sims/main.cpp
@@ -53,7 +53,7 @@ void yousefs_simulation(int experiment_n, json conditions)
 	double v_y = std::sqrt(velocity_magnitude*velocity_magnitude/2);
 	double v[2] = {v_x,v_y};
 	//double t_max = conditions[std::to_string(experiment_n)]["t_max"];
-	double t_max = 6;
+	double t_max = 1.0;
 	
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -62,7 +62,7 @@ void yousefs_simulation(int experiment_n, json conditions)
 	// Outputs
 	bool save_vtk = false;
 	bool save_hdf5 = false;
-	bool save_mass = true;
+	bool save_mass = false;
 	bool save_csv = true;
 	std::string output_folder = "/output_all_simulations/";
 	
@@ -96,65 +96,64 @@ void yousefs_simulation(int experiment_n, json conditions)
 	auto phi_epsilon = std::numeric_limits<phi_type>::epsilon();
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// OpenFPM Grid Initialization
+	// Initialize OpenFPM grid
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	auto & v_cl = create_vcluster();
-	
-	// Grid property indices		
-	constexpr size_t
-		CONC_N                 = 0,
-		CONC_NPLUS1            = 1,
-		CONC_LAP               = 2,
-		CONC_N_GRAD            = 3,
-		K_SOURCE               = 4,
-		K_SINK                 = 5,
-		PHI_N                  = 6,  // level-set function Phi
-		PHI_NPLUS1             = 7,  // level-set function Phi of next timepoint
-		VELOCITY               = 8,  // velocity vector field
-		PHI_GRAD               = 9,  // gradient of phi (vector field)
-		PHI_GRAD_MAGNITUDE     = 10; // Eucledian norm of gradient (scalar field)
-	
 	// Create grid of size NxN
 	Box<dims, double> box({box_lower, box_lower}, {box_upper, box_upper});
 	Ghost<dims, long int> ghost(1);
-	typedef aggregate<double, 			// CONC_N
-					  double, 			// CONC_NPLUS1
-					  double, 			// CONC_LAP
-					  double[dims], 	// CONC_N_GRAD
-					  double, 			// K_SOURCE
-					  double, 			// K_SINK
-					  double, 			// PHI_N
-					  double, 			// PHI_NPLUS1
-					  double[dims], 	// VELOCITY
-					  double[dims], 	// PHI_GRAD
-					  double			// PHI_GRAD_MAGNITUDE
-					  > props;
+	
+	// Grid property indices		
+	constexpr size_t
+							CONC_N                 = 0,
+							CONC_NPLUS1            = 1,
+							CONC_LAP               = 2,
+							CONC_N_GRAD            = 3,
+							K_SOURCE               = 4,
+							K_SINK                 = 5,
+							PHI_N                  = 6,  	// level-set function Phi
+							PHI_NPLUS1             = 7,  	// level-set function Phi of next timepoint
+							VELOCITY               = 8,  	// velocity vector field
+							PHI_GRAD               = 9,  	
+							PHI_GRAD_MAGNITUDE     = 10; 	// Eucledian norm of gradient (scalar field)
+	typedef aggregate<  	double, 						// CONC_N
+							double, 						// CONC_NPLUS1
+							double, 						// CONC_LAP
+							double[dims], 					// CONC_N_GRAD
+							double, 						// K_SOURCE
+							double, 						// K_SINK
+							double, 						// PHI_N
+							double, 						// PHI_NPLUS1
+							double[dims], 					// VELOCITY
+							double[dims], 					// PHI_GRAD
+							double							// PHI_GRAD_MAGNITUDE
+							> props;
 	typedef grid_dist_id<dims, double, props > grid_type;
 	grid_type g_dist(sz, box, ghost);
-	g_dist.setPropNames({"CONC_N", 
-						"CONC_NPLUS1", 
-						"CONC_LAP", 
-						"CONC_N_GRAD", 
-						"K_SOURCE", 
-						"K_SINK", 
-						"PHI_N", 
-						"PHI_NPLUS1", 
-						"VELOCITY", 
-						"PHI_GRAD", 
-						"PHI_GRAD_MAGNITUDE"});
+	g_dist.setPropNames({	"CONC_N", 
+							"CONC_NPLUS1", 
+							"CONC_LAP", 
+							"CONC_N_GRAD", 
+							"K_SOURCE", 
+							"K_SINK", 
+							"PHI_N", 
+							"PHI_NPLUS1", 
+							"VELOCITY", 
+							"PHI_GRAD", 
+							"PHI_GRAD_MAGNITUDE"});
 	
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Initialize grid values
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// init_grid_and_ghost<VELOCITY>(g_dist, v);
 	init_grid_and_ghost<PHI_N>(g_dist, -1);
 	init_grid_and_ghost<PHI_NPLUS1>(g_dist, -1);
 	init_grid_and_ghost<CONC_N>(g_dist, 0);
 	init_grid_and_ghost<CONC_NPLUS1>(g_dist, 0);
 	init_analytic_sdf_circle<PHI_N>(g_dist, radius, center[x], center[y]); // Level-set function
-	// Phi gradient & gradient magnitude
-	if (growth_on || advection_on){
+	
+	if (growth_on || advection_on) // Phi gradient & gradient magnitude
+	{
 		get_upwind_gradient<PHI_N, VELOCITY, PHI_GRAD>(g_dist, 1, true);
 		get_vector_magnitude<PHI_GRAD, PHI_GRAD_MAGNITUDE, double>(g_dist);
 	}
@@ -178,7 +177,6 @@ void yousefs_simulation(int experiment_n, json conditions)
             x_max = x;
             y_max = y;
         } 
-            
 		++dom_phi_max;
 	}
 	
@@ -197,7 +195,7 @@ void yousefs_simulation(int experiment_n, json conditions)
 			auto phi_grad = g_dist.template getProp<PHI_GRAD>(key);
 			// Calculate unit vector of phi gradient to get correct magnitude & direction of velocity
 			// Vector multiplication in case we want unequal velocities between x & y
-			double v_adjusted[dims] = {v[0]*phi_grad[0]/phi_grad_mag, v[1]*phi_grad[1]/phi_grad_mag};
+			double v_adjusted[dims] = {v[0]*phi_grad[0], v[1]*phi_grad[1]/phi_grad_mag};
 			g_dist.template getProp<VELOCITY>(key)[0] = v_adjusted[0];
 			g_dist.template getProp<VELOCITY>(key)[1] = v_adjusted[1];
 		}
@@ -271,8 +269,7 @@ void yousefs_simulation(int experiment_n, json conditions)
 	// Check and print initial mass after initializing concentrations
 	const double dx = g_dist.spacing(x), dy = g_dist.spacing(y); // Get grid spacings
 	double p_volume  = dx * dy;
-	double m_initial = sum_prop_over_region<CONC_N, PHI_N>(g_dist, emb_boundary) * p_volume; // Initial total mass
-	if (m_initial == 0){m_initial = 0.01;} // is this needed?
+	double m_initial = std::max(sum_prop_over_region<CONC_N, PHI_N>(g_dist, emb_boundary) * p_volume, 0.01); // Initial total mass
 	
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -282,70 +279,62 @@ void yousefs_simulation(int experiment_n, json conditions)
 	const double dt_dif = get_diffusion_time_step(g_dist, D);
 	const double dt = dt_dif;
 	if (growth_on || advection_on){
-		// doing this manually b/c function doesn't seem to work with array
-		//auto max_v = *std::max_element(std::begin(v), std::end(v));
-		//const double dt_adv = get_advection_time_step_cfl(g_dist, max_v, 0.1);
-		double sum = 0;
-		for (size_t d = 0; d < dims; d++)
-		{
-			sum += v[d] / g_dist.spacing(d);
-		}
-		const double dt_adv = 0.1 / abs(sum);
-		// Final dt is min of the two
-		const double dt = std::min(dt_adv, dt_dif);
+		grid_type::stype v_grid_type[dims] = {v[0], v[1]}; // need to make grid type v for function to work
+		double dt_adv = get_advection_time_step_cfl(g_dist, v_grid_type, 0.1);
+		const double dt = std::min(dt_adv, dt_dif); // Final dt is min of the two
 	}
 	int max_iter = (int)(t_max / dt) + 1;
-	int interval_write = (int)(max_iter / 100); // set how many frames should be saved as vtk
+	int interval_write = std::round(max_iter / 100); // set how many frames should be saved
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Saving
+	// Configure outputs and save initial conditions
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	std::string cwd = get_cwd();
 	const std::string path_output = cwd + output_folder;
 	create_directory_if_not_exist(path_output);
-	std::string save_path = "sim_" + std::to_string(experiment_n);
-	if (save_vtk){g_dist.write(path_output + "grid_initial", FORMAT_BINARY);}
+	std::string save_name = "sim_" + std::to_string(experiment_n);
+	
 	// Initialize csv writer
 	std::ofstream file_out;
 	std::string csv_row = "";
 	if (save_csv){ // but don't create file unless save_csv is on
-		std::string csv_path_output = path_output + save_path + ".csv";
+		std::string csv_path_output = path_output + save_name + ".csv";
 		create_file_if_not_exist(csv_path_output);
 		file_out.open(csv_path_output, std::ios_base::app);
-		// Fill first two rows with simulation settings
-		file_out << 
-		"diffusion," << 
-		"growth," << 
-		"advection," <<
-		"source_sink_condition," <<
-		"max_iter," << 
-		"interval_write," << 
-		"diffusion_coefficient," << 
-		"source_val," << 
-		"sink_val," <<
-		"v_x," << 
-		"v_y," << 
-		"t_max" << 
-		"dt," << 
-		"dx," <<
-		"dy" << "\n" << 
+		file_out << // Fill first two rows with simulation settings
+		"diffusion," 							<< 
+		"growth," 								<< 
+		"advection," 							<<
+		"source_sink_condition," 				<<
+		"max_iter," 							<< 
+		"interval_write," 						<< 
+		"grid_length," 							<<
+		"diffusion_coefficient," 				<< 
+		"source_val," 							<< 
+		"sink_val," 							<<
+		"v_x," 									<< 
+		"v_y," 									<< 
+		"dt," 									<< 
+		"t_max,"								<<
+		"dx," 									<<
+		"dy" 									<< "\n" <<
 		
-		diffusion_on << "," << 
-		growth_on << "," << 
-		advection_on << "," << 
-		source_sink_cond << "," <<
-		max_iter << "," << 
-		interval_write << "," << 
-		to_string_with_precision(D, 3) << "," << 
-		to_string_with_precision(k_source, 3) << "," << 
-		to_string_with_precision(k_sink, 3) << "," <<
-		to_string_with_precision(v[0], 3) << "," << 
-		to_string_with_precision(v[1], 3) << "," << 
-		to_string_with_precision(t_max,2) << "," <<
-		to_string_with_precision(dt, 6) << "," << 
-		to_string_with_precision(dx, 6) << 
-		to_string_with_precision(dy, 6) << 
-		"\n" << "\n";
+		diffusion_on 							<< "," << 
+		growth_on 								<< "," << 
+		advection_on 							<< "," << 
+		source_sink_cond 						<< "," <<
+		max_iter 								<< "," << 
+		interval_write 							<< "," << 
+		N 										<< "," <<
+		to_string_with_precision(D, 3) 			<< "," << 
+		to_string_with_precision(k_source, 3) 	<< "," << 
+		to_string_with_precision(k_sink, 3) 	<< "," <<
+		to_string_with_precision(v[0], 3) 		<< "," << 
+		to_string_with_precision(v[1], 3) 		<< "," << 
+		to_string_with_precision(dt, 6) 		<< "," << 
+		to_string_with_precision(t_max, 6) 		<< "," << 
+		to_string_with_precision(dx, 6) 		<< "," << 
+		to_string_with_precision(dy, 6) 		<< "\n\n";
 	}
 	
 	
@@ -418,7 +407,7 @@ void yousefs_simulation(int experiment_n, json conditions)
 				// Determine if redistancing needs to be run based on # of grid points 
 				if (growth_on){
 					auto phi_grad_mag = g_dist.template get<PHI_GRAD_MAGNITUDE>(key); //get the magnitude of the phi gradient
-					if (phi_grad_mag > 1.2 || phi_grad_mag < 0.8){
+					if (phi_grad_mag > 1.15 || phi_grad_mag < 0.8){
 						phi_grad_tol_break += 1;
 					}
 				}
@@ -448,15 +437,11 @@ void yousefs_simulation(int experiment_n, json conditions)
 																+ dt*ksource_here - dt*ksink_here*u;            
 					}
 					else if (diffusion_on && advection_on){
-						auto phi_grad_mag = g_dist.template get<PHI_GRAD_MAGNITUDE>(key);
-						auto phi_grad = g_dist.template getProp<PHI_GRAD>(key);
-						// Calculate unit vector of phi gradient to get correct magnitude & direction of velocity
-						// Vector multiplication in case we want unequal velocities between x & y
-						double v_adjusted[dims] = {v[0]*phi_grad[0]/phi_grad_mag, v[1]*phi_grad[1]/phi_grad_mag};
 						auto grad_u = g_dist.template get<CONC_N_GRAD>(key);
+						auto v_here = g_dist.template get<VELOCITY>(key);
 						double v_advec = 0;
-						// Dot product with concentration gradient to get final advection term
-						for(size_t d = 0; d < dims; d++){v_advec += v_adjusted[d] * grad_u[d];}
+						// Dot product with concentration gradient to get final advection term						
+						for(size_t d = 0; d < dims; d++){v_advec += v_here[d] * grad_u[d];}
 						g_dist.template get<CONC_NPLUS1>(key) = u+ D*dt*laplacian_u + dt*ksource_here 
 																- dt*ksink_here*u + dt*v_advec;
 					}
@@ -466,22 +451,21 @@ void yousefs_simulation(int experiment_n, json conditions)
 			copy_gridTogrid<CONC_NPLUS1, CONC_N>(g_dist, g_dist); // Update CONC_N
 		} // End diffusion
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		if (growth_on){
-			if (phi_grad_tol_break > 20) {	
+			if (phi_grad_tol_break > 10) {	
 				Redist_options<phi_type> redist_options;
 				redist_options.min_iter                             = 1e3;
 				redist_options.max_iter                             = 1e4;
-		
 				redist_options.convTolChange.value                  = 1e-7;
 				redist_options.convTolChange.check                  = true;
-				redist_options.convTolResidual.value                = 1e-6; // is ignored if convTolResidual.check = false;
+				redist_options.convTolResidual.value                = 1e-6;//is ignored if convTolResidual.check=false;
 				redist_options.convTolResidual.check                = false;
-		
 				redist_options.interval_check_convergence           = 1e3;
 				redist_options.width_NB_in_grid_points              = 10;
-				redist_options.print_current_iterChangeResidual     = true;
+				redist_options.print_current_iterChangeResidual     = false;
 				redist_options.print_steadyState_iter               = true;
-				redist_options.save_temp_grid                       = true;
+				redist_options.save_temp_grid                       = false;
 				
 				// Redistancing
 				RedistancingSussman<grid_type, phi_type> redist_obj(g_dist, redist_options); 
@@ -496,7 +480,19 @@ void yousefs_simulation(int experiment_n, json conditions)
 			while(phi_dom_iter.isNext()) // looping over the grid points
 			{
 				auto key = phi_dom_iter.get();
-				g_dist.template get<PHI_NPLUS1>(key) = g_dist.template get<PHI_N>(key) + dt * v[0] * g_dist.template get<PHI_GRAD_MAGNITUDE>(key);
+				
+				auto phi_mag = g_dist.template getProp<PHI_GRAD_MAGNITUDE>(key);
+				g_dist.template get<PHI_NPLUS1>(key) = g_dist.template get<PHI_N>(key) + dt * v[0] * phi_mag;
+				
+				
+				/*
+				double dot_v_dphi = 0;
+				auto v_here = g_dist.template get<VELOCITY>(key);
+				auto grad_phi = g_dist.template getProp<PHI_GRAD>(key);
+				for(size_t d = 0; d < dims; d++){dot_v_dphi += v_here[d] * grad_phi[d];}
+				g_dist.template get<PHI_NPLUS1>(key) = g_dist.template get<PHI_N>(key) + dt * dot_v_dphi;
+				*/
+				
 				++phi_dom_iter;
 			}
 			// Update PHI_N
@@ -510,9 +506,9 @@ void yousefs_simulation(int experiment_n, json conditions)
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// Write grid to vtk and/or hdf5; save mass monitoring file
 		if (iter % interval_write == 0){
-			if (save_vtk){g_dist.write_frame(path_output + save_path, iter, FORMAT_BINARY);}
-			if (save_hdf5){g_dist.save(path_output + save_path + std::to_string(iter) + ".hdf5");}
-			if (save_mass){monitor_absolute_mass_over_region<CONC_N, PHI_N>(g_dist, emb_boundary, m_initial, p_volume, t, iter, path_output, save_path + "_mass.csv");}
+			if (save_vtk){g_dist.write_frame(path_output + save_name, iter, FORMAT_BINARY);}
+			if (save_hdf5){g_dist.save(path_output + save_name + std::to_string(iter) + ".hdf5");}
+			if (save_mass){monitor_absolute_mass_over_region<CONC_N, PHI_N>(g_dist, emb_boundary, m_initial, p_volume, t, iter, path_output, save_name + "_mass.csv");}
 			if (save_csv){file_out << csv_row << std::endl;}
 		}
 		iter += 1;
